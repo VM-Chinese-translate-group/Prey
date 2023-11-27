69fe6c67f6ea8e609cba79ab3b79f29ced2ea2b0
CHEN Prime <chenbprime@outlook.com>
2023-11-27 19:33:14 +0800

    test

--- source_files/packer.py
+++ source_files/packer.py
@@ -11,7 +11,7 @@ def run_packaging():
     content = config.get('content')
     # 获取打包结果的输出路径
     output_path = config.get('output_path')
-    
+    # dwdmao
     # 执行打包操作
     # 根据您的需求自行编写打包逻辑，这里只是一个示例
     command = f'your_packaging_command {content} {output_path}'
